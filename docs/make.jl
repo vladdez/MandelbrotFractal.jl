a4da4fb8d837a00eb1dc14f342aa253bbf789855
Vladimir Mikheev <33777074+vladdez@users.noreply.github.com>
2023-03-29 13:23:14 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -11,7 +11,7 @@ makedocs(;
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://vlmikheev.github.io/MandelbrotFractal.jl",
-        edit_link="master",
+        edit_link="main",
         assets=String[],
     ),
     pages=[
@@ -21,5 +21,5 @@ makedocs(;
 
 deploydocs(;
     repo="github.com/vlmikheev/MandelbrotFractal.jl",
-    devbranch="master",
+    devbranch="main",
 )
