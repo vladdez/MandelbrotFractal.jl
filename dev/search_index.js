7213c9c298562638c981881c22ebd22c2b0e826d
Documenter.jl <documenter@juliadocs.github.io>
2023-03-29 11:31:52 +0000

    build based on 1ca15de

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MandelbrotFractal","category":"page"},{"location":"#MandelbrotFractal","page":"Home","title":"MandelbrotFractal","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MandelbrotFractal.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MandelbrotFractal]","category":"page"}]
+}
